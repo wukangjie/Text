d9069297fa9b5b46d1292a2b23e827d6bda79dbc
wubihang <dllo@kangjiebaba.local>
2016-09-08 10:30:59 +0800

    no message

--- FirstGit/app/src/main/java/com/example/dllo/firstgit/MainActivity.java
+++ FirstGit/app/src/main/java/com/example/dllo/firstgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //功能一
+        //功能一  ggggg
     }
 }
